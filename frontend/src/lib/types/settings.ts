3617720518f0bdfc4282a7b5844508f3b7da60ec
Kyle Mendell <ksm@ofkm.us>
2026-08-25 23:02:10 -0500

    feat: add copacetic direct image patching (#3744)

--- frontend/src/lib/types/settings.ts
+++ frontend/src/lib/types/settings.ts
@@ -79,6 +79,11 @@ export type Settings = {
 	trivyServerUrl: string;
 	trivyServerToken: string;
 	trivyIgnoreUnfixed: boolean;
+	imagePatchSuffix: string;
+	imagePatchTimeoutSec: number;
+	imagePatchAllPlatforms: boolean;
+	imageAutoPatchEnabled: boolean;
+	imageAutoPatchInterval: string;
 	lifecycleEnabled: boolean;
 	lifecycleDefaultRunnerImage: string;
 	lifecycleMaxTimeoutSec: number;
@@ -161,6 +166,7 @@ export type JobSchedules = {
 	pollingInterval: string;
 	scheduledPruneInterval: string;
 	vulnerabilityScanInterval: string;
+	imageAutoPatchInterval: string;
 	autoHealInterval: string;
 };
 
